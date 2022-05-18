2d4585e4486d84999db9d5250e18d61603c250f7
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-05-18 13:49:26 +0200

    Work in progress: Added almost all ontclasses

--- src/main/java/com/sdm/knowledge_graph/ontology/TBOX.java
+++ src/main/java/com/sdm/knowledge_graph/ontology/TBOX.java
@@ -17,17 +17,64 @@ public class TBOX {
         // https://jena.apache.org/documentation/javadoc/jena/org/apache/jena/rdf/model/ModelFactory.html#createOntologyModel(org.apache.jena.ontology.OntModelSpec)
         OntModel model = ModelFactory.createOntologyModel( OntModelSpec.RDFS_MEM_RDFS_INF );
 
-        OntClass paper = model.createClass( constants.BASE_URI.concat("Paper") );
+        //==================================
+        // For Persons
+        //==================================
+        
         OntClass person = model.createClass( constants.BASE_URI.concat("Person") );
         OntClass author = model.createClass( constants.BASE_URI.concat("Author") );
         OntClass chair = model.createClass( constants.BASE_URI.concat("Chair") );
         OntClass editor = model.createClass( constants.BASE_URI.concat("Editor") );
         OntClass reviewer = model.createClass( constants.BASE_URI.concat("Reviewer") );
-
+        
         person.addSubClass( author );
         person.addSubClass( chair );
         person.addSubClass( editor );
         person.addSubClass( reviewer );
+        
+        //==================================
+        // For Papers
+        //==================================
+
+        OntClass paper = model.createClass( constants.BASE_URI.concat("Paper") );
+        OntClass fullPaper = model.createClass( constants.BASE_URI.concat("Full_Paper") );
+        OntClass shortPaper = model.createClass( constants.BASE_URI.concat("Short_Paper") );
+        OntClass demoPaper = model.createClass( constants.BASE_URI.concat("Demo_Paper") );
+        OntClass posterPaper = model.createClass( constants.BASE_URI.concat("Poster_Paper") );
+        
+        paper.addSubClass( fullPaper );
+        paper.addSubClass( shortPaper );
+        paper.addSubClass( demoPaper );
+        paper.addSubClass( posterPaper ); // Only for conference papers
+        
+        //==================================
+        // For Venue
+        //==================================
+        
+        OntClass venue = model.createClass( constants.BASE_URI.concat("Venue") );
+        OntClass conference = model.createClass( constants.BASE_URI.concat("Conference") );
+        OntClass journal = model.createClass( constants.BASE_URI.concat("Journal") );
+        
+        venue.addSubClass( conference );
+        venue.addSubClass( journal );
+
+        //==================================
+        // For Venue
+        //==================================
+
+        OntClass venue = model.createClass( constants.BASE_URI.concat("Venue") );
+        OntClass conference = model.createClass( constants.BASE_URI.concat("Conference") );
+        OntClass journal = model.createClass( constants.BASE_URI.concat("Journal") );
+        
+        venue.addSubClass( conference );
+        venue.addSubClass( journal );
+
+
+
+
+
+
+
 
         model.write(System.out);
 
