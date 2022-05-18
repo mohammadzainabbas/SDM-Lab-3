6a617d67e18a717ee064a5bc77717df7e3443af7
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-05-18 12:54:12 +0200

    Work in progress

--- src/main/java/com/sdm/knowledge_graph/ontology/TBOX.java
+++ src/main/java/com/sdm/knowledge_graph/ontology/TBOX.java
@@ -1,10 +1,23 @@
 package com.sdm.knowledge_graph.ontology;
 
-// import com.apache.jena
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntClass;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.rdf.model.ModelFactory;
+
 
 public class TBOX {
 
     public static void createTBOX() {
+
+        // https://jena.apache.org/documentation/javadoc/jena/org/apache/jena/ontology/OntModel.html
+        // https://jena.apache.org/documentation/javadoc/jena/org/apache/jena/ontology/OntModelSpec.html
+        // https://jena.apache.org/documentation/javadoc/jena/org/apache/jena/rdf/model/ModelFactory.html#createOntologyModel(org.apache.jena.ontology.OntModelSpec)
+        OntModel model = ModelFactory.createOntologyModel( OntModelSpec.RDFS_MEM_RDFS_INF );
+
+        OntClass paper = model.createClass( );
+
+
 
     }
 }
