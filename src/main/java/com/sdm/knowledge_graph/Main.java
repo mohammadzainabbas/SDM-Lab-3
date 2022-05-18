61fb76094bac9e6684ad5310d890b1ee89269403
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-05-18 13:38:34 +0200

    Minor

--- src/main/java/com/sdm/knowledge_graph/Main.java
+++ src/main/java/com/sdm/knowledge_graph/Main.java
@@ -1,13 +1,26 @@
 package com.sdm.knowledge_graph;
 
+import com.sdm.knowledge_graph.ontology.TBOX;
+import com.sdm.knowledge_graph.common.utils;
+
 /**
- * Hello world!
- *
+ * Main class
  */
 public class Main 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        utils.print( "Hola from the main class!" );
+        utils.print( "args: " + String.join(" ", args) );
+        utils.print( "args.length: " + args.length );
+        
+        if (args.length != 1) {
+            utils.error("Parameter expected: Pass 'tbox' or 'abox' for creating TBOX and ABOX respectively");
+        }
+
+        if (args[0].equals("tbox")) {
+            TBOX.createAndSaveTBOX();
+        }
+
     }
 }
