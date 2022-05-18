69cc52dc7e835431e261955bd81e77575004d9cf
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-05-18 13:39:11 +0200

    Work in progress for createAndSaveTBOX

--- src/main/java/com/sdm/knowledge_graph/ontology/TBOX.java
+++ src/main/java/com/sdm/knowledge_graph/ontology/TBOX.java
@@ -5,19 +5,31 @@ import org.apache.jena.ontology.OntClass;
 import org.apache.jena.ontology.OntModelSpec;
 import org.apache.jena.rdf.model.ModelFactory;
 
+import com.sdm.knowledge_graph.common.constants;
+import com.sdm.knowledge_graph.common.utils;
 
 public class TBOX {
 
-    public static void createTBOX() {
+    public static void createAndSaveTBOX() {
 
         // https://jena.apache.org/documentation/javadoc/jena/org/apache/jena/ontology/OntModel.html
         // https://jena.apache.org/documentation/javadoc/jena/org/apache/jena/ontology/OntModelSpec.html
         // https://jena.apache.org/documentation/javadoc/jena/org/apache/jena/rdf/model/ModelFactory.html#createOntologyModel(org.apache.jena.ontology.OntModelSpec)
         OntModel model = ModelFactory.createOntologyModel( OntModelSpec.RDFS_MEM_RDFS_INF );
 
-        OntClass paper = model.createClass( );
+        OntClass paper = model.createClass( constants.BASE_URI.concat("Paper") );
+        OntClass person = model.createClass( constants.BASE_URI.concat("Person") );
+        OntClass author = model.createClass( constants.BASE_URI.concat("Author") );
+        OntClass chair = model.createClass( constants.BASE_URI.concat("Chair") );
+        OntClass editor = model.createClass( constants.BASE_URI.concat("Editor") );
+        OntClass reviewer = model.createClass( constants.BASE_URI.concat("Reviewer") );
 
+        person.addSubClass( author );
+        person.addSubClass( chair );
+        person.addSubClass( editor );
+        person.addSubClass( reviewer );
 
+        model.write(System.out);
 
     }
 }
