511329828926351361076442507efc7449304700
unknown <tejdhuppad@gmail.com>
2022-05-19 23:11:32 +0200

    File added

---
+++ src/main/java/com/sdm/knowledge_graph/ontology/ABOX.java
@@ -0,0 +1,14 @@
+package com.sdm.knowledge_graph.ontology;
+
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntClass;
+import org.apache.jena.ontology.OntProperty;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.rdf.model.ModelFactory;
+
+import com.sdm.knowledge_graph.common.constants;
+import com.sdm.knowledge_graph.common.utils;
+
+public class ABOX {
+    
+}
