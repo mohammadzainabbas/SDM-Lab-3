02f73fb43b81215962e2785ba07896d74723f4a9
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-05-20 15:15:27 +0200

    Fixes the issue with ABOX

--- src/main/java/com/sdm/knowledge_graph/ontology/ABOX.java
+++ src/main/java/com/sdm/knowledge_graph/ontology/ABOX.java
@@ -6,6 +6,8 @@ import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntClass;
@@ -20,57 +22,6 @@ import com.sdm.knowledge_graph.common.utils;
 
 public class ABOX {
    
-    // public static void createPerson() throws IOException {
-    ///     Model model = ModelFactory.createDefaultModel();
-
-    //     // Read the csv line by line
-    //     BufferedReader csvReader = new BufferedReader(new FileReader(FILE_PATH));
-    //     String row;
-    //     while ((row = csvReader.readLine()) != null) {
-    //         String[] row_data = row.split(";");
-    //         String[] names = row_data[0].split(" ");
-    //         String[] papers = row_data[1].split("\\|");
-
-    //         String lastName = names[names.length-1];
-
-    //         String firstName = names[0];
-    //         String personUri = names[0];
-    //         for(String name:names){
-    //             if(!(name.equals(lastName) || name.equals(firstName))){
-    //                 firstName += " "+name;
-    //                 personUri += "_"+name;
-    //             }
-    //         }
-
-    //         String reviewedPaper = row_data[4];
-    //         personUri = RESOURCE_URL + personUri + lastName;
-
-    //         String workplaceUri = RESOURCE_URL+row_data[2].replace(" ","_");
-
-    //         Resource currentPerson = model.createResource(personUri)
-    //                 .addProperty(FOAF.firstName, firstName)
-    //                 .addProperty(FOAF.lastName, lastName)
-    //                 .addProperty(model.createProperty(Config.PROPERTY_URL+"works_in"),model.createResource(workplaceUri));
-
-    //         for(String paper:papers){
-    //             currentPerson.addProperty(model.createProperty(Config.PROPERTY_URL+"writes"), model.createResource(Config.RESOURCE_URL+paper.replace("/","_")));
-    //         }
-    //         if(!(reviewedPaper.equals("N/A"))){
-    //             for(String paper:reviewedPaper.split("\\|")){
-    //                 currentPerson.addProperty(model.createProperty(Config.PROPERTY_URL+"reviews"),
-    //                         model.createResource(Config.RESOURCE_URL+row_data[0].replace(" ","_")+"_"+paper
-    //                                 .replace("/","_")));
-    //             }
-    //         }
-
-    //     }
-    //     csvReader.close();
-    //     model.write(new PrintStream(
-    //             new BufferedOutputStream(
-    //                     new FileOutputStream(OUTPUT_PATH+"person.nt")), true), "NT");
-    // }
-
-
     public static void createAndSaveABOX() {
 
         Model m = ModelFactory.createDefaultModel().read(constants.MODEL_PATH);
@@ -80,13 +31,25 @@ public class ABOX {
        // Read the csv file 
        //===============================
 
-        BufferedReader csvReader = new BufferedReader(new FileReader(constants.FILE_PATH));
-        CSVParser parser = CSVFormat.DEFAULT.withDelimiter(',').withHeader().parse(br);
-        for(CSVRecord record : parser) {
-            utils.print(record.get("Author"));
-            String author_name = record.get("Author");
-            author.createIndividual( constants.BASE_URI.concat(author_name) );
-            model.write(System.out);
-        }
+        try {
+            utils.line_separator();
+            utils.print("Reading instances data from '" + constants.FILE_PATH + "'");
+            
+            BufferedReader csvReader = new BufferedReader(new FileReader(constants.FILE_PATH));
+            CSVParser parser = CSVFormat.DEFAULT.withDelimiter(',').withHeader().parse(csvReader);
+            for(CSVRecord record : parser) {
+                // utils.print(record.get("Author"));
+                String author_name = record.get("Author");
+                author.createIndividual( constants.BASE_URI.concat(author_name) );
+                model.write(System.out);
+            }
+
+            utils.print("Done with ABOX creation!");
+            utils.line_separator();
+        } catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 }
