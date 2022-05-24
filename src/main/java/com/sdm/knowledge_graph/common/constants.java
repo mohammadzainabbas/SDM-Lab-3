92ecbee158601f94beab40f271842b835d20fbae
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-05-24 15:11:53 +0200

    Minor fix

--- src/main/java/com/sdm/knowledge_graph/common/constants.java
+++ src/main/java/com/sdm/knowledge_graph/common/constants.java
@@ -5,6 +5,6 @@ import java.lang.String;
 public class constants {
     public static final String BASE_URI = "http://www.bdma.upc/#";
     public static final String MODEL_PATH = "data/publications.owl";
-    public static final String DATA_PATH = "data/publications_data.nl";
+    public static final String DATA_PATH = "data/publications_data.nt";
     public static final String FILE_PATH = "data/raw/dummy_data.csv";
 }
