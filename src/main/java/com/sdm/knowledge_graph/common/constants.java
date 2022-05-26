6163adecae6deb563eeb80cf1c3367bba7a9d715
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-05-26 05:30:54 +0200

    Reverted back to original: .rdfs file extension not supported by graphdb

--- src/main/java/com/sdm/knowledge_graph/common/constants.java
+++ src/main/java/com/sdm/knowledge_graph/common/constants.java
@@ -4,7 +4,7 @@ import java.lang.String;
 
 public class constants {
     public static final String BASE_URI = "http://www.bdma.upc/#";
-    public static final String TBOX_MODEL_PATH = "data/publications.rdfs";
+    public static final String TBOX_MODEL_PATH = "data/publications.owl";
     public static final String ABOX_MODEL_PATH = "data/publications_data.nt";
     public static final String DATA_FILE_PATH = "data/raw/instances_data.csv";
 }
